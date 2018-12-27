156c3d552737d4383b6644538c862f09673b05cb
nathanielpascual <nath_pascual@yahoo.com>
2018-12-27 21:39:11 +0800

    initial first

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {LoggingService} from '../shared/logging.service';
+import { AccountService } from '../shared/account.service';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService]
+})
+export class AccountComponent {
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+  
+  constructor(private loggingService: LoggingService,
+              private accountService: AccountService) {
+
+  }
+
+  onSetTo(status: string) {
+    this.accountService.updateStatus(this.id,status);
+    this.loggingService.logStatusChange(status);
+  }
+}
---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter } from '@angular/core';
+import {LoggingService} from '../shared/logging.service';
+import {AccountService} from '../shared/account.service';
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
+})
+export class NewAccountComponent {
+  constructor(private loggingService: LoggingService,
+              private accountService: AccountService) {
+
+  }
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountService.addAccount(accountName,accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
+  }
+}
